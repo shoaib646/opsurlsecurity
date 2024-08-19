ed886f137b213cc2b0b65e89cba684713a477290
Shoaib646 <shoaibs2346@gmail.com>
2024-08-19 14:21:50 -0400

    Day 2

---
+++ SecurityFolder/pipeline/TrainingPipeline.py
@@ -0,0 +1,2 @@
+class TrainigPipeline:
+    pass
---
+++ Training.py
@@ -0,0 +1,17 @@
+import os, sys
+from SecurityFolder.exception.exception import NetworkException
+from SecurityFolder.logger.logger import *
+
+from SecurityFolder.pipeline.TrainingPipeline import TrainingPipeline
+
+
+def main():
+    try:
+        pass
+    except Exception as e:
+        raise Exception(e, sys)
+
+
+
+if __name__ == '__main__':
+    main()
