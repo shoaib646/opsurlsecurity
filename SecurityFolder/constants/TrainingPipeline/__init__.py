b77d54074150c73107ebccfdacc5da3f8c6ad760
Shoaib646 <shoaibs2346@gmail.com>
2024-08-21 14:37:31 -0400

    D2 Completed- Test

--- SecurityFolder/constants/TrainingPipeline/__init__.py
+++ SecurityFolder/constants/TrainingPipeline/__init__.py
@@ -40,15 +40,10 @@ DATABASE_NAME : str  = os.getenv('MONGO_DB')
 Data Ingestion related constant variables
 '''
 
-
 DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 DATA_INGESTION_INGESTED_DIR: str = "ingested"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
-
-
-
-
 
 
 
