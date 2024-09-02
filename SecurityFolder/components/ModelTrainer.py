ded8ab3f620f71647eb5325f9fed5f8e2eb7af45
Shoaib646 <shoaibs2346@gmail.com>
2024-09-02 00:21:14 -0400

    model trainer updated

--- SecurityFolder/components/ModelTrainer.py
+++ SecurityFolder/components/ModelTrainer.py
@@ -1,7 +1,15 @@
+from SecurityFolder.exception.exception import NetworkException
+from SecurityFolder.logger.logger import logging
 
+from SecurityFolder.entities.artifacts import DataTransformationArtifact, ModelTrainerArtifact
+from SecurityFolder.entities.config import ModelTrainerConfig
 
+import os, sys
+from xgboost import  XGBClassifier
 
-
+from SecurityFolder.utils.ML.model.estimator import NetworkModel
+from SecurityFolder.utils.Main.utils import save_object, load_object, load_numpy_array_data
 
 class ModelTrainer:
-    pass
+    def __init__(self):
+        pass
